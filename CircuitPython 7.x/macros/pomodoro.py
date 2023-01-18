c31acd161c8d417c5e160c448119211ec4af0ae8
Navindu <navanavi3@gmail.com>
2023-01-18 09:40:31 +0530

    feat: time tracking

--- CircuitPython 7.x/macros/pomodoro.py
+++ CircuitPython 7.x/macros/pomodoro.py
@@ -13,26 +13,24 @@ from adafruit_display_text import label
 
 # initializing data
 data = {
+    "boot_at": None,
     "start_time": None,
     "paused_at": 0,
     "offset": 0,  # used for pausing and resuming
     "session_count": 0,
-    "session_ended": False
+    "session_ended": False,
+    "total_focused": 0,
+    "total_sessions": 0,
 }
 
 # constants
 WORK_SESSION = {
     "duration": 25 * 60,
-    "title": "FOCUS"
+    "title": "FOCUS",
+    "focus": True,  # Required to calculate "Total sessions" and "total focused"
 }
-SHORT_BREAK = {
-    "duration": 5 * 60,
-    "title": "SHORT BREAK"
-}
-LONG_BREAK = {
-    "duration": 15 * 60,
-    "title": "LONG BREAK"
-}
+SHORT_BREAK = {"duration": 5 * 60, "title": "SHORT BREAK", "focus": False}
+LONG_BREAK = {"duration": 15 * 60, "title": "LONG BREAK", "focus": False}
 
 sequence = [
     WORK_SESSION,
@@ -42,11 +40,41 @@ sequence = [
     WORK_SESSION,
     SHORT_BREAK,
     WORK_SESSION,
-    LONG_BREAK
+    LONG_BREAK,
 ]
 
 
+def seconds_to_hours_minutes(seconds):
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    return (hours, minutes)
+
+
+def lightPulse(macropad):
+    for i in range(0, 8):
+        macropad.pixels.fill((0, 0, 0))
+        macropad.pixels.show()
+
+        rowId = (i % 4) * 3
+        macropad.pixels[rowId] = (255, 255, 255)
+        macropad.pixels[rowId + 1] = (255, 255, 255)
+        macropad.pixels[rowId + 2] = (255, 255, 255)
+        macropad.pixels.show()
+        time.sleep(0.1)
+
+    # reset to original colors
+    macropad.pixels.fill((0, 0, 0))
+    macropad.pixels[0] = (27, 148, 0)
+    macropad.pixels[1] = (252, 186, 3)
+    macropad.pixels[2] = (255, 0, 0)
+    macropad.pixels.show()
+
+
 def Pomodoro(macropad, encoder_position):
+    # Save boot time to later calculate the "slacking" time
+    if data["boot_at"] is None:
+        data["boot_at"] = time.time()
+    # Set neopixel colors
     macropad.pixels.fill((0, 0, 0))
 
     macropad.pixels[0] = (27, 148, 0)
@@ -73,6 +101,7 @@ def Pomodoro(macropad, encoder_position):
             terminalio.FONT,
             text="",
             color=0xFFFFFF,
+            line_spacing=0.8,
             anchored_position=(
                 macropad.display.width / 2,
                 macropad.display.height / 2,
@@ -113,14 +142,23 @@ def Pomodoro(macropad, encoder_position):
                     data["paused_at"] = 0
                     data["offset"] = 0
                     data["session_ended"] = False
+
+                    # prolly need a cleaner way to do this
+                    group[2].scale = 2
                 if key_number == 1:
                     if data["paused_at"] > 0:
-                        data["offset"] = data["offset"] + \
-                            (data["paused_at"] - data["start_time"])
+                        data["offset"] = data["offset"] + (
+                            data["paused_at"] - data["start_time"]
+                        )
                         data["start_time"] = time.time()
                         data["paused_at"] = 0
                     else:
                         data["paused_at"] = time.time()
+                if key_number == 2:
+                    data["session_ended"] = True
+                    diff = 0
+                    data["session_count"] = data["session_count"] + 1
+                    lightPulse(macropad)
 
         # exit -------------------------------------
         pos = macropad.encoder
@@ -129,9 +167,25 @@ def Pomodoro(macropad, encoder_position):
             return
 
         # Calculate time difference ---------------
-        if not data["start_time"] == None:
+        if not data["start_time"] is None:
+            idx = data["session_count"] % len(sequence)
+
             if data["session_ended"]:
-                group[2].text = "00:00"
+                _h, _m = seconds_to_hours_minutes(data["total_focused"])
+
+                slacking_dur = time.time() - \
+                    data["boot_at"] - data["total_focused"]
+                slack_h, slack_m = seconds_to_hours_minutes(slacking_dur)
+
+                stat_text = f"{_h}h {_m}m focused ({data['total_sessions']})"
+                stat_text = stat_text + f"\n {slack_h}h {slack_m}m slacking"
+
+                group[2].text = stat_text
+
+                group[2].scale = 1
+
+                group[3].text = f"Next: {sequence[idx]['title']}"
+                macropad.display.refresh()
                 continue
 
             if data["paused_at"] > 0:
@@ -139,15 +193,27 @@ def Pomodoro(macropad, encoder_position):
             else:
                 diff = time.time() - data["start_time"] + data["offset"]
 
-            idx = data["session_count"] % len(sequence)
-
             diff = sequence[idx]["duration"] - diff
 
-            if(diff <= 0):
-                macropad.play_tone(262, 0.5)
+            # this means the timer has ended
+            if diff <= 0:
+                macropad.play_tone(255, 1)
+                macropad.play_tone(255, 1)
+
                 data["session_ended"] = True
                 data["session_count"] = data["session_count"] + 1
+
+                if sequence[idx]["focus"]:
+                    data["total_focused"] = (
+                        data["total_focused"] + WORK_SESSION["duration"]
+                    )
+                    data["total_sessions"] = data["total_sessions"] + 1
+
                 diff = 0
+
+                # Do a little visual indication
+                lightPulse(macropad)
+                lightPulse(macropad)
 
             minutes = diff // 60
             seconds = diff % 60
@@ -162,9 +228,8 @@ def Pomodoro(macropad, encoder_position):
             macropad.display.refresh()
 
 
-app = {
-    'name': 'Pomodoro',
-    'macros': [],
-    'custom_func': Pomodoro,
-    'custom_data': data
-}
+app = {"name": "Pomodoro",
+       "macros": [],
+       "custom_func": Pomodoro,
+       "custom_data": data
+       }
